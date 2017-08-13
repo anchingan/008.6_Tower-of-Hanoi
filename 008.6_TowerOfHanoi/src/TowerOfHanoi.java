19899df1d2d8b0e26dc943b27161fc426fd3b12c
chingan <chingan@192.168.11.121>
2017-08-14 00:00:10 +0800

    Ver.1 Running example of 3 plates.: Please input numbers of tower of hanoi: 3 Move no.1 plate from 1 to 2. Move no.2 plate from 1 to 3. Move no.1 plate from 1 to 3. Move no.3 plate from 1 to 3. Move no.1 plate from 1 to 3. Move no.2 plate from 1 to 2. Move no.1 plate from 1 to 2. Program terminate.

--- 008.6_TowerOfHanoi/src/TowerOfHanoi.java
+++ 008.6_TowerOfHanoi/src/TowerOfHanoi.java
@@ -2,11 +2,35 @@
  * Practice 008.6_Tower of hanoi
  * Date 20170813
  */
+
+import java.util.Scanner;
+
 public class TowerOfHanoi {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please input numbers of tower of hanoi: ");
+		int number = scanner.nextInt();
+		towersOfHanoi(number, 1, 3, 2);
+		
+		System.out.println("Program terminate.");
 
+	}
+	
+	public static void towersOfHanoi(int no, int from, int to, int temp) {
+		if (no == 1)
+			System.out.printf("Move no.%d plate from %d to %d.\n", no, from, to);
+		else if (no % 2 == 0) {
+			towersOfHanoi((no - 1), from, temp, to);
+			System.out.printf("Move no.%d plate from %d to %d.\n", no, from, to);
+			towersOfHanoi((no - 1), from, to, temp);
+		}
+		else {
+			towersOfHanoi((no - 1), from, to, temp);
+			System.out.printf("Move no.%d plate from %d to %d.\n", no, from, to);
+			towersOfHanoi((no - 1), from, temp, to);
+		}
 	}
 
 }
