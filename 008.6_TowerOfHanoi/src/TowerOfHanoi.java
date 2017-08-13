5009b3d8ef90744b0fdb2f6ee2a6bf52b343d0f3
chingan <chingan@192.168.11.121>
2017-08-13 15:01:00 +0800

    河內塔（Tower of Hanoi） 請觀看以下網頁了解河內塔的玩法規則 https://zh.wikipedia.org/wiki/汉诺塔 共有三根柱子，編號從1號到3號，柱子1號已經排好n個盤子，盤子從上到下依照尺寸由小到大排列，編號為1, 2, ..., n，你的目標是將所有的盤子移動到3號柱子上，但是你必須遵守以下規則： 1.	每次只能從最上面移動一個盤子 2.	任何盤子可以從任何柱子搬到其他柱子 3.	小盤子永遠必須放在大盤子之上 請撰寫一個遞迴函式用來描述整個河內塔的移動過程。遞迴函式的函式原型如下： public static void towersOfHanoi(int no, int from, int to, int temp); 參數代表要從柱子from移動編號no的盤子到柱子to去，同時以柱子temp作為中介 只要有移動一個盤子，請印出以下訊息： Move 盤子編號 from 來源柱子編號 to 來源柱子編號 請撰寫一個主函式要求使用者輸入一個數字n代表一開始要移動的盤子總數，並呼叫towersOfHanoi來印出搬移的過程。

--- 008.6_TowerOfHanoi/src/TowerOfHanoi.java
+++ 008.6_TowerOfHanoi/src/TowerOfHanoi.java
@@ -1,8 +1,11 @@
-
+/*
+ * Practice 008.6_Tower of hanoi
+ * Date 20170813
+ */
 public class TowerOfHanoi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
